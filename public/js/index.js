dcfe6d9142fdd09b56dcaa57cb96bb07f4e54de1
fanzhe <1161087471@qq.com>
2019-03-03 17:39:22 +0800

    增加axios请求数据

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,17 @@
-const request = require("request");
-
-function init(){
-    document.getElementById("root").text("111")
+function init() {
+    axios.get("/data").then(res => {
+        if (res.data) {
+            let data = res.data;
+            var para = document.createElement("p");
+            var node = document.createTextNode("这是新段落。");
+            para.appendChild(node);
+            var element = document.getElementById("root");
+            element.appendChild(para);
+        }
+    })
 }
 
 
-init();
+window.onload = function () {
+    init(root)
+};
