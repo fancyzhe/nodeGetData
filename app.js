668a125e08838b9445702acc1176d24b6ad16ae9
fanzhe <1161087471@qq.com>
2019-03-03 17:17:05 +0800

    init

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cheerio = require("cheerio");
+const request =  require("request");
+const Router = express.Router();
+const path = require('path')
+
+
+
+const app = express();
+const url = "https://movie.douban.com/";
+
+//请求页面的数据
+function getData(){
+    let data =[];
+    return new Promise((resolve,reject)=>{
+        request(url,(err,res,body)=>{
+            $ = cheerio.load(body);
+            let content = $("#screening .ui-slide-item");
+            let length = content.length;
+            while(length--){
+                let title = $(content[length]).attr("data-title");
+                title && data.push(title);
+            }
+            resolve(data)
+        })
+    })
+}
+
+app.get("/data",(req,res)=>{
+    console.log("getData")
+    getData().then(data=>{
+        res.send(data)
+    })
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000,()=>console.log("run 3000"))
---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+const request = require("request");
+
+function init(){
+    document.getElementById("root").text("111")
+}
+
+
+init();
